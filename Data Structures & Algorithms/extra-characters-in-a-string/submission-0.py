91899e0d69e02f9337bc09b93789eb6db77fcbb3
Yefan(Lucy) He <121675209+yefanh@users.noreply.github.com>
2026-09-24 18:22:15 -0400

    Add: extra-characters-in-a-string - submission-0

---
+++ Data Structures & Algorithms/extra-characters-in-a-string/submission-0.py
@@ -0,0 +1,36 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.isWord = True
+
+class Solution:
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        trie = Trie()
+        for w in dictionary:
+            trie.addWord(w)
+        dp = {len(s) : 0}
+        def dfs(i: int) -> int:
+            if i in dp:
+                return dp[i]
+            res = 1 + dfs(i + 1)
+            curr = trie.root
+            for j in range(i, len(s)):
+                if s[j] not in curr.children:
+                    break
+                curr = curr.children[s[j]]
+                if curr.isWord:
+                    res = min(res, dfs(j + 1))
+            dp[i] = res
+            return res
+        return dfs(0) 
